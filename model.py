1e9186e44431ae10444e81012dbb13517b93c781
Thainá Foltran <thainafoltran@hotmail.com>
2025-09-09 23:49:16 -0300

    add funçao teste cadastro pet

--- model.py
+++ model.py
@@ -14,3 +14,32 @@ class Animais():
         self.localizaçao = localizaçao
         self.idade = idade
 
+from flask import jsonify
+
+#só teste
+pets_cadastrados = []
+
+class Animais():
+    __tablename__ = "pets"
+
+
+    def __init__(self, nome_pet, raça, especie, localizaçao, idade):
+        self.nome_pet = nome_pet
+        self.raça = raça
+        self.especie = especie #gato ou cachorro. não sei se tira ou deixa
+        self.localizaçao = localizaçao
+        self.idade = idade
+
+#funções 
+
+def cadastrar_pet(add_pet):
+    novo_pet = Animais(
+        nome_pet=add_pet['nome do pet'],
+        raça=add_pet['raça'],
+        especie=add_pet['especie'],
+        localizaçao=add_pet['localizaçao'],
+        idade=add_pet['idade']
+    )
+
+    pets_cadastrados.append(novo_pet)
+    return novo_pet
