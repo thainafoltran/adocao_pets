ce48e6c2115bfab40979a1ab047f7feb31aad84a
Lucas Leite <lukinhas.leite2011@gmail.com>
2025-09-11 11:35:36 -0300

    terminando rotas, config e bd

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-from route import pet_bp
-
-
-app = Flask(__name__)
+from config import app, db
+from .animal.animal_route import pet_bp 
 
 app.register_blueprint(pet_bp)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlachemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['HOST'] = '127.0.0.1'
