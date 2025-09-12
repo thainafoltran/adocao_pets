007a77dfbd79aa6e417ad96f6c3741b19e36a3ce
PEDRO SANTOS <pedrohenos600@gmail.com>
2025-09-12 14:34:30 -0300

    Cria front-end, estrutura sem layout, main e cadastro-pet

---
+++ front-end/js/cadastro-animal.js
@@ -0,0 +1,35 @@
+const url = 'http://127.0.0.1:5000/users/pets'
+
+async function cadastroPet(){
+
+    let nome = document.getElementById('nome').value
+    let especie = document.getElementById('especie').value
+    let localizacao = document.getElementById('localizacao').value
+    let idade = document.getElementById('idade').value
+
+    let api = await fetch(url,{
+        method:"POST",
+        body:JSON.stringify(
+            {
+                "nome":nome,
+                "especie":especie,
+                "localizacao":localizacao,
+                "idade":idade
+            }
+        ),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+
+    if(api.ok){
+        let resposta = await api.json();
+        console.log(resposta)
+        alert('Cadastro Realizado!')
+        return
+    }
+
+    let respostaErro =  await api.json();
+    console.log(respostaErro)
+
+}
